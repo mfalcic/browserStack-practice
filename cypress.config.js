ee0e050de7d8de557c8876b3030baa990dbe959a
Milan Falcic <m.falcic@vegait.rs>
2024-01-27 16:44:19 +0100

    Deleted dependecies

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,5 @@
 const bundler  = require('@bahmutov/cypress-esbuild-preprocessor')
 const { defineConfig } = require("cypress");
-const cucumber = require('cypress-cucumber-preprocessor').default;
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
-
-// module.exports = defineConfig({
-//   setupNodeEvents(on, config) {
-//       on('file:preprocessor',cucumber())
-//     return config;
-//   },
-// })
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
@@ -44,13 +34,9 @@ module.exports = defineConfig({
       },
       "video": false,
     setupNodeEvents(on, config) {
-      preprocessor.addCucumberPreprocessorPlugin(on, config);
-      // on("file:preprocessor", browserify.default(config));
       on('file:preprocessor', bundler())
-      on('file:preprocessor', cucumber())
       require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     },
-    specPattern: "cypress/e2e/*.feature",
   },
 });
